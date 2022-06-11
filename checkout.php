c6a1887dfeb3afa1c5ce32d727f92721ef74aab4
emanuelElCrack <75333105+emanuelElCrack@users.noreply.github.com>
2022-06-10 21:04:29 -0500

    Carrito_Funciona_Con_Cantidades

--- checkout.php
+++ checkout.php
@@ -5,6 +5,20 @@ require 'config/database.php';
 $db = new DataBase();
 $con = $db->conectar();
 
+$articulos = isset($_SESSION['carrito']['articulos']) ? $_SESSION['carrito']['articulos'] : null;
+
+//print_r($_SESSION);
+$lista_carrito = array();
+
+if ($articulos != null) {
+    foreach ($articulos as $clave => $cantidad) {
+        $sql = $con->prepare("SELECT idArticulo, nombre, foto, precio, descripcion, $cantidad as cantidad FROM articulos where idArticulo=? AND cantidadEnAlmacen >=1");
+        $sql->execute([$clave]);
+        $lista_carrito[] = $sql->fetch(PDO::FETCH_ASSOC);
+    }
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -36,8 +50,9 @@ $con = $db->conectar();
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                     </ul>
-
-                    <!--a href="carrito.php" class="btn btn-primary">Carrito</a-->
+                    <a href="checkout.php" class="btn btn-primary">
+                        Carrito <span id="num_cart" class="badge bg-secondary"><?php echo $num_cart ?></span>
+                    </a>
                 </div>
 
             </div>
@@ -63,10 +78,133 @@ $con = $db->conectar();
             </div>
         </section>
 
+        <div class="container">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Producto</th>
+                            <th>Foto</th>
+                            <th>Precio</th>
+                            <th>Cantidad</th>
+                            <th>Subtotal</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if ($lista_carrito == null) {
+                            echo '<tr><td colspan="6" class="text-center"><b>Lista vacia</b></td></tr>';
+                        } else {
+                            $total = 0;
+                            foreach ($lista_carrito as $articulo) {
+                                $idArticulo = $articulo['idArticulo'];
+                                $nombre = $articulo['nombre'];
+                                $imagen = $articulo['foto'];
+                                $precio = $articulo['precio'];
+                                $cantidad = $articulo['cantidad'];
+                                $descripcion = $articulo['descripcion'];
+                                $subtotal = $precio * $cantidad;
+                                $total += $subtotal;
+
+                        ?>
+                                <tr>
+                                    <td><?php echo $nombre; ?></td>
+                                    <td><img width="50" height="80" src="data:image;base64, <?php echo $imagen = base64_encode($imagen) ?>"></td>
+                                    <td>Precio: <?php echo MONEDA . number_format($precio, 2, '.', ','); ?> MXN</td>
+                                    <td>
+                                        <div class="form-floating mb-3">
+                                            <input type="number" class="form-control rounded-3" id="cantidad_<?php echo $idArticulo; ?>" placeholder="cantidad" name="cantidad" min="1" max="99999" maxlength="5" value="<?php echo $cantidad; ?>" size="3" onchange="actualizaCantidad(this.value,<?php echo $idArticulo; ?>)">
+                                            <label for="floatingPassword">Cantidad </label>
+
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div id="subtotal_<?php echo $idArticulo; ?>" name="subtotal[]">
+                                            <?php echo MONEDA . number_format($subtotal, 2, '.', ','); ?> MXN
+                                        </div>
+                                    </td>
+                                    <td><a href="#" id="eliminar" class="btn btn-warning btn-sm" data-bs-id="<?php echo $idArticulo; ?>" data-bs-toggle="modal" data-bs-target="#eliminaModal">
+                                            Eliminar
+                                        </a></td>
+                                </tr>
+                            <?php } ?>
+
+                            <tr>
+                                <td colspan="4"></td>
+                                <td colspan="2">
+                                    <p class="h3" id="total"><?php echo MONEDA . number_format($total, 2, '.', ','); ?></p>
+                                </td>
+                            </tr>
+                    </tbody>
+                <?php } ?>
+                </table>
+
+            </div>
+
+        </div>
+
     </main>
+
+    <!-- Modal -->
+    <div class="modal fade" id="eliminaModal" tabindex="-1" aria-labelledby="eliminaModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="eliminaModalLabel">Alerta</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ¿Desea eliminar el producto de la lista?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button id="btn-eliminar" type="button" class="btn btn-danger" onclick="elimina">Eliminar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script>
+        let eliminaModal = document.getElementById('eliminaModal')
+        eliminaModal.addEventListener('')
+
+        function actualizaCantidad(cantidad, idArticulo) {
+            let url = 'clases/actualizar_carrito.php'
+            let formData = new FormData()
+            formData.append('idArticulo', idArticulo)
+            formData.append('action', 'agregar')
+            formData.append('cantidad', cantidad);
+
+            fetch(url, {
+                    method: 'POST',
+                    body: formData,
+                    mode: 'cors'
+                }).then(response => response.json())
+                .then(data => {
+                    if (data.ok) {
+                        let divsubtotal = document.getElementById('subtotal_' + idArticulo)
+                        divsubtotal.innerHTML = data.sub
+
+                        let total = 0.00
+                        let list = document.getElementsByName('subtotal[]')
+
+                        for (i = 0; i < list.length; i++) {
+                            total += parseFloat(list[i].innerHTML.replace(/[$,]/g, ''))
+                        }
+                        total = new Intl.NumberFormat('en-US', {
+                            minimumFractionDigits: 2
+
+                        }).format(total)
+                        document.getElementById('total').innerHTML = '<?php MONEDA;  ?>' + total
+                    }
+                })
+
+        }
+    </script>
+
 </body>
 
 </html>
---
+++ clases/actualizar_carrito.php
@@ -0,0 +1,51 @@
+<?php
+    require '../config/config.php';
+    require '../config/database.php';
+    
+    if(isset($_POST['action'])){
+        $action = $_POST['action'];
+        $idArticulo = isset($_POST['idArticulo']) ? $_POST['idArticulo'] : 0;
+
+        if($action=='agregar'){
+            $cantidad = isset($_POST['cantidad']) ? $_POST['cantidad'] : 0;
+            $respuesta = agregar($idArticulo,$cantidad);
+            if($respuesta>0){
+                $datos['ok']=true;
+            }else{
+                $datos['ok']=false;
+            }
+            $datos['sub'] = MONEDA . number_format($respuesta,2,'.',',');
+        }else{
+            $datos['ok']=false;
+        }
+    }else{
+        $datos['ok']=false;
+    }
+
+    echo json_encode($datos);
+
+
+    function agregar($idArticulo,$cantidad){
+        $res = 0;
+        if($idArticulo >0 && $cantidad>0 && is_numeric(($cantidad))){
+            if(isset($_SESSION['carrito']['articulos'][$idArticulo])){
+                $_SESSION['carrito']['articulos'][$idArticulo] = $cantidad;
+
+                $db = new DataBase();
+                $con = $db->conectar();
+
+            $sql = $con->prepare("SELECT precio FROM articulos where idArticulo=? AND cantidadEnAlmacen >=1");
+            $sql->execute([$idArticulo]);
+            $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($resultado as $row)
+           
+            $precio = $row['precio'];
+            
+            $res = $cantidad * $precio;
+            return $res;
+            }
+        }else{
+            return $res;
+        }
+    }
+?>
